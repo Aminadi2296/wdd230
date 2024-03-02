def2319105d653161f1339c1ffec306e4fd77440
Aminadi2296 <moroniaminadi@gmail.com>
2024-03-02 16:00:40 -0400

    w01 Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+document.getElementById("placeholder").innerHTML = new Date(document.lastModified);
+
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
